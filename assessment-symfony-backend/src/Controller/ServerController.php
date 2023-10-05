15ae2197be9a9a6ebf8d79582deb4a0bcd7c0030
Alex <alexxsur@gmail.com>
2023-10-05 16:04:58 -0400

    Use XlsxServerDataHandler in controller

--- assessment-symfony-backend/src/Controller/ServerController.php
+++ assessment-symfony-backend/src/Controller/ServerController.php
@@ -4,18 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\XlsxDatabaseService;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\XlsxDatabaseService;
+use App\Class\XlsxServerDataHandler;
 
 class ServerController extends AbstractController
 {
     #[Route('/server', name: 'server_list', methods:['GET'])]
     public function list(XlsxDatabaseService $xlsxDatabase)
     {
-        // Read data from the XLSX file
+        // Read data from the xlsx file
         $data = $xlsxDatabase->readData();
 
-        return new Response(serialize($data));
+        // Format xlsx data
+        $xlsx_handler = new XlsxServerDataHandler();
+        $formated_data = $xlsx_handler->formatArrayData($data);
+
+        return new Response(serialize($formated_data));
     }
 }
