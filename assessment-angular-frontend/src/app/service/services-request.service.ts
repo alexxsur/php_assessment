75da06641a46ca83366ca9989f54fcd59f1038ec
Alex <alexxsur@gmail.com>
2023-10-07 17:56:09 -0400

    build urlServerQueryString

--- assessment-angular-frontend/src/app/service/services-request.service.ts
+++ assessment-angular-frontend/src/app/service/services-request.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject, filter } from 'rxjs';
+import { Subject} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -7,12 +7,55 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ServicesRequestService {
   subject = new Subject();
-  constructor(private httpRequest: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   public searchByFilter(filter:any){
-    //this.httpRequest.get<any>();
-    console.log(filter);
-    this.subject.next("LA LISTAAA");
+    //console.log(filter);
+
+    let ramFilterString = '';
+    let ramCheckedValues = filter.ramCheckFilter.filter((ramCheckItem: any) => ramCheckItem.active);
+
+    for (const item of ramCheckedValues) {
+        ramFilterString = `${ramFilterString}${ramFilterString != '' ? '&' : ''}ram[]=${item.ramCapacity}`;
+    }
+
+    let apiHost = 'http://localhost:8000';
+    let apiEndpoint = '/server';
+    let filtersString = '';
+
+    console.log(ramFilterString);
+
+    if (ramFilterString !== '' && filter.hddType !== '' && filter.location !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&hdd_type=${filter.hddType}&location=${filter.location}&${ramFilterString}`;
+    } else if (ramFilterString !== '' && filter.hddType !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&hdd_type=${filter.hddType}&${ramFilterString}`;
+    } else if (filter.location !== '' && filter.hddType !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&hdd_type=${filter.hddType}&location=${filter.location}`;
+    } else if (ramFilterString !== '' && filter.location !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&location=${filter.location}&${ramFilterString}`;
+    } else if (ramFilterString !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&${ramFilterString}`;
+    } else if (filter.hddType !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&hdd_type=${filter.hddType}`;
+    } else if (filter.location !== '') {
+      filtersString = `?storage=${filter.rangeStorage}&location=${filter.location}`;
+    }
+
+    let urlServerQueryString = `${apiHost}${apiEndpoint}${filtersString}`;
+
+    console.log(urlServerQueryString);
+
+    let dataList;
+
+    this.http.get<any>(urlServerQueryString).subscribe(data => {
+        console.log(data);
+        dataList = data;
+        //console.log(dataList.results);
+    })
+
+    let itemList : any[] = [];
+
+    this.subject.next(itemList);
   }
 
   public getObservable(): Subject<any>{
